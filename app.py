27c40d68a8face5d72f291f5126377a68fc158db
tuansonmancom-lab <tuanson.mancom@gmail.com>
2026-09-10 11:42:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ import os
 from datetime import datetime
 from io import BytesIO
 
-from streamlit_autorefresh import st_autorefresh
-
-# Run every 5000 milliseconds (5 seconds)
-count = st_autorefresh(interval=5000, limit=None, key="datarefresh")
 
 # --- REPORTLAB PDF GENERATION LIBRARIES ---
 try:
