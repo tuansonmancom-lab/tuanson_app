d2c9c363b141ff7482326e554ede637ee3c906cd
tuansonmancom-lab <tuanson.mancom@gmail.com>
2026-09-18 15:39:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def cheque_amount_to_words(amount):
         words_str = " ".join(parts)
 
     if cents > 0:
-        return f"*** {words_str} & {cents:02d}/100 PESOS ONLY ***"
-    return f"*** {words_str} PESOS ONLY ***"
+        return f"{words_str} & "PESOS" & {cents:02d}/100."
+    return f"{words_str} PESOS"
 
 import os
 from reportlab.pdfbase import pdfmetrics
